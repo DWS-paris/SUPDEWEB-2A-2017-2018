decbaad7c66a136085d2aa388832b6841dfd85f8
Julien Noyer <dwsparis@MacBook-Pro-de-Julien.local>
2018-02-14 09:55:37 +0100

    Modification du service

--- TODOang/src/app/services/tasks.service.ts
+++ TODOang/src/app/services/tasks.service.ts
@@ -2,47 +2,54 @@
 Importer les class
 */
   import { Injectable } from '@angular/core';
-  import { TaskModel } from '../models/task.model'
-//
 
+  // Importer la class Http et Response pour faire des requêtes
+  import { Http, Response } from '@angular/http';
+
+  // Importer le sytème de gestion de Promises
+  import 'rxjs/add/operator/toPromise';
+
+  // Importer le model de données
+  import { TaskModel } from '../models/task.model';
+//
 
 
 /*
 Définir le service
 */
   @Injectable()
+
   export class TasksService {
-    /*
-    Création d'une collection de données
-    */  
-      public dataCollection: Array<TaskModel> = [
-        {
-          id: 0,
-          content: `Créer des composants`,
-          isDone: true
-        },
-        {
-          id: 1,
-          content: `Créer un servive`,
-          isDone: true
-        },
-        {
-          id: 2,
-          content: `Afficher les tâches dans le dashboard`,
-          isDone: false
-        }
-      ];
-    //
-    
 
-    constructor() { }
+  /*
+  Définir l'adresse de l'API
+  */
+    private apiUrl = `http://localhost:3000/tasks`;
+  //
+  
+  // Injecter la class Http dans le constructeur
+  constructor( private http: Http ){}
 
-    /*
-    Créer une fonction pour renvoyer la collection de données
-    */
-      public showTasks = (): Array<TaskModel> => {
-        return this.dataCollection
-      }
-    //
+
+
+
+  /*
+  Fonctions de traitement des requêtes sur l'API
+  */
+    // Traitement des réponses JSON
+    private dataFromApi(res: Response) {
+      return res.json() || { };
+    }
+
+
+    // Traitement des erreurs
+    private handleError (error: any) {
+      let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+      
+      return Promise.reject(errMsg);
+    }
+  //
+
   }
 //
